764cedf45c57fb780a609b6a5812e238efcba2d2
Kasia <katarzynamakarowicz@gmail.com>
2023-08-22 14:40:13 +0200

    feat: giard design website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./src/**/*.{html,js}'], 
+    
+    theme: {
+        extend: {
+            backgroundImage: {
+                hero: "url('/public/hero.jpg')",
+                firmie: "url('/public/firmie.jpg')",
+            },
+
+            colors: {
+                'green-main': '#1B5B31',
+                'grey-main': '#F5F0EC',
+                'beige': '#DCC1AB',
+            },
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+                monte: ['Montserrat', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
